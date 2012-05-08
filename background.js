f274079e5d1688d33269b2e638d843b981cf4240
David West <david.matthew.west@gmail.com>
2012-05-08 14:54:32 -0700

    Initial files

---
+++ background.js
@@ -0,0 +1,31 @@
+var reload_tabs = function() {  
+  chrome.windows.getAll({ populate: true }, function(windows)
+  {
+    windows.forEach(function(window)
+    {
+      window.tabs.forEach(function(tab)
+      {
+        console.log(tab);
+        if (/fedev/.test(tab.url))
+        {
+          console.log("Reloading " + tab.url);
+          chrome.tabs.reload(tab.id);
+        }
+      });
+    });
+  });
+};
+
+var check = function()
+{
+  $.getJSON('http://fedev.utah.trulia.com/~dwest/refreshment/refresh.php',
+            function(data)
+            {
+              if (data.success)
+              {
+                reload_tabs();
+              }
+              setTimeout(check, 1000);       
+            });
+};
+check();
